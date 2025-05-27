4927ed3feed9794867b687c03f43ac9226b9e65d
xanaxhol1c <loram.hoptan@gmail.com>
2025-05-27 18:04:28 +0300

    created dockerfiles for backend and frontend

--- coinradar/coinradar/settings.py
+++ coinradar/coinradar/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0']
 
 
 # Application definition
